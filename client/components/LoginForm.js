97b71cad517adff711c9c8590de17ff32e475f70
chaddoy <capdumadag@gmail.com>
2025-03-12 16:36:54 +0800

    add login error catch

--- client/components/LoginForm.js
+++ client/components/LoginForm.js
@@ -15,6 +15,9 @@ class LoginForm extends Component {
     this.props.mutate({
       variables: { email, password },
       refetchQueries: [{ query }]
+    }).catch(res => {
+      const errors = res.graphQLErrors.map(error => error.message);
+      console.log(errors)
     });
   }
 
