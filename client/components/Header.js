555852dcef1f713eb3130e8f8ea6b7a129af4e28
chaddoy <capdumadag@gmail.com>
2025-03-12 16:04:36 +0800

    add logout mutation and logout onclick action

--- client/components/Header.js
+++ client/components/Header.js
@@ -2,15 +2,31 @@ import React from 'react';
 import { graphql } from 'react-apollo';
 import { Link } from 'react-router';
 import query from '../queries/CurrentUser';
+import mutation from '../mutations/Logout';
 
 class Header extends React.Component {
+  onLogoutClick() {
+    this.props.mutate({
+      refetchQueries: [{ query }],
+    });
+  }
+
   renderButtons() {
     const { loading, user } = this.props.data;
     if (loading) {
       return <div />;
     }
     if (user) {
-      return <div>Logout</div>;
+      return (
+        <div>
+          <li>
+            <Link to="/dashboard">Dashboard</Link>
+          </li>
+          <li>
+            <a onClick={this.onLogoutClick.bind(this)}>Logout</a>
+          </li>
+        </div>
+      );
     } else {
       return (
         <div>
@@ -32,13 +48,11 @@ class Header extends React.Component {
           <Link to="/" className="brand-logo left">
             Home
           </Link>
-          <ul className="right">
-            <li>{this.renderButtons()}</li>
-          </ul>
+          <ul className="right">{this.renderButtons()}</ul>
         </div>
       </nav>
     );
   }
 }
 
-export default graphql(query)(Header);
+export default graphql(mutation)(graphql(query)(Header));
